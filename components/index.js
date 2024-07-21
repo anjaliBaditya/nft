3baf83b31fdf0fdba0461d39009e702b194ddcd8
anjaliBaditya <anjalibaditya8@gmail.com>
2024-07-21 19:31:18 +0000

    learning native

---
+++ components/index.js
@@ -0,0 +1,16 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { CircleButton, RectButton } from "./Button";
+
+export {
+	HomeHeader,
+	FocusedStatusBar,
+	NFTCard,
+	DetailsBid,
+	DetailsDesc,
+	CircleButton,
+	RectButton,
+};
